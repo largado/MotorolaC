967b0831981127f51ad6a6b24e42698b55deeec2
largado <alexandrohipolito@gmail.com>
2022-06-18 20:27:52 -0300

    versão 0.1

--- app/src/main/java/avena/com/MainActivity.java
+++ app/src/main/java/avena/com/MainActivity.java
@@ -2,6 +2,7 @@ package avena.com;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -10,7 +11,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "seja bem vindo!!!!", Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_main);
     }
 }
 
